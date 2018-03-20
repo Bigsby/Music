7bdcc9922c8ac7f9f4e9da349494ace104c96562
Bruno Bernardo <bigsby@live.com>
2018-03-20 17:48:55 +0000

    Start adding Theory module

---
+++ site/src/app/theory/theory.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./_component/appcomponent";
+
+@NgModule({
+    declarations: [
+        AppComponent
+        //   RoutingComponents
+    ],
+    imports: [
+        //   BrowserModule,
+        //   AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class TheoryModule { }
