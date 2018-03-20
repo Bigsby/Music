7fd1eb948cda986094c36079a87c7bd46cc64c9a
Bruno Bernardo <bigsby@live.com>
2018-03-20 16:43:23 +0000

    Scafold site

---
+++ site/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
+
+import { AppComponent } from './_component/app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoutingComponents
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
