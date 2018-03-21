fa7df6d2f529e9baa2ccf03b4948d3968b6b9803
Bruno Bernardo <bigsby@live.com>
2018-03-21 14:40:09 +0000

    Complete Not Found

--- site/src/app/app.components/not-found.component.ts
+++ site/src/app/app.components/not-found.component.ts
@@ -2,37 +2,38 @@ import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute, NavigationExtras } from "@angular/router";
 
 @Component({
+    template: ""
 })
 export class NotFoundComponent implements OnInit {
     constructor(private router: Router, private activeRoute: ActivatedRoute) {
     }
 
     ngOnInit(): void {
-        let path = this.activeRoute.queryParams["path"] as string;
-        let queryParams = this.activeRoute.queryParams["query"] as string;
+        this.activeRoute.queryParams.subscribe(params => {
+            let path = decodeURI(params.path || "");
+            let queryParams = decodeURI(params.query || "");
 
-        if (!path) {
-            this.router.navigate([""]);
-        } else {
-            let pathParts = decodeURI(path).substr(1).split("/");
+            if (!path) {
+                this.router.navigate([""]);
+            } else {
+                let pathParts = decodeURI(path).split("/");
 
-            let query = "";
-            let extras: NavigationExtras = {
-                queryParams: {}
-            };
+                let extras: NavigationExtras = {
+                    queryParams: {}
+                };
 
-            if (query) {
-                query = decodeURI(queryParams).substr(1);
-                query.split("&").forEach(p => {
-                    var pair = p.split("=");
-                    extras.queryParams[pair[0]] = pair[1];
-                });
+                if (queryParams) {
+                    queryParams.split("&").forEach(p => {
+                        var pair = p.split("=");
+                        extras.queryParams[pair[0]] = pair[1];
+                    });
+                }
+
+                let routePath = pathParts[0];
+                let routeRemain = pathParts.splice(1);
+                routeRemain.unshift("/" + routePath);
+                this.router.navigate(routeRemain, extras);
             }
-
-            let routePath = pathParts[0];
-            let routeRemain = pathParts.splice(1);
-            routeRemain.unshift("/" + routePath);
-            this.router.navigate(routeRemain, extras);
-        }
+        });
     }
 }
